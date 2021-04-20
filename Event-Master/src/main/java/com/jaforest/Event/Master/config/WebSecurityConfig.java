195fa125fdc11554743575b6a71e1bae0b47d2ea
Barrett <baxance@protonmail.com>
2021-04-20 09:34:23 -0700

    working login on home page

--- Event-Master/src/main/java/com/jaforest/Event/Master/config/WebSecurityConfig.java
+++ Event-Master/src/main/java/com/jaforest/Event/Master/config/WebSecurityConfig.java
@@ -10,10 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     @Autowired
     UserDetailsServiceImplementation userDetailsService;
