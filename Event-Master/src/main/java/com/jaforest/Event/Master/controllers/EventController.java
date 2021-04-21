d53fe118b453a483997b1c0e6af276124ab4d0c4
Barrett <baxance@protonmail.com>
2021-04-20 20:50:46 -0700

    eventDB -> json

--- Event-Master/src/main/java/com/jaforest/Event/Master/controllers/EventController.java
+++ Event-Master/src/main/java/com/jaforest/Event/Master/controllers/EventController.java
@@ -1,5 +1,6 @@
 package com.jaforest.Event.Master.controllers;
 
+import com.google.gson.Gson;
 import com.jaforest.Event.Master.model.event.Event;
 import com.jaforest.Event.Master.model.event.EventRepository;
 import com.jaforest.Event.Master.model.family.Family;
@@ -11,6 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.security.Principal;
 
 @Controller
@@ -37,7 +41,23 @@ public class EventController {
         event.setTitle(title);
         event.setFamilyEventsIBelongTo(family);
         eventRepository.save(event);
+        eventRepository.findById(member.getFamilyIBelongTo().getId());
 
+        /////////////////////////////////////
+
+        Gson gson = new Gson();
+        String path = "Event-Master/src/main/java/com/jaforest/Event/Master/gson/events.json";
+        File file = new File(path);
+        FileWriter fileWriter = null;
+
+        try {
+            fileWriter = new FileWriter(file);
+            gson.toJson(event, fileWriter);
+            fileWriter.close();
+            System.out.println("writing event to file");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new RedirectView ("/calendar");
     }
 }
