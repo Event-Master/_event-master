1fcffb92c0a9113af7dfd1404c18b943c504c0f4
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-04-22 00:21:21 -0400

    Randomized images and made dynamic events populate

--- Event-Master/src/main/resources/static/js/profile.js
+++ Event-Master/src/main/resources/static/js/profile.js
@@ -1,31 +1,46 @@
-var calendarEl = $('#calendar-profile');
 
-calendarEl.fullCalendar({
- header:
-     {
-          left: 'prev,next today',
-          center: 'title',
-          right: 'listMonth,listWeek,listDay'
-     },
-    defaultView: 'listWeek',
-    events: [
-    {
-        title: 'All Day Event',
-        start: '2021-04-20'
-    },
-    {
-        title: 'Long Event',
-        start: '2020-04-21',
-        end: '2020-04-24'
-    },
-    {
-        id: 999,
-        title: 'Repeating Event',
-        start: '2020-12-09T16:00:00'
-    }]});
+var familyPics = [
+    "/img/starfamily.jpeg","/img/phone-thieves.jpg",
+    "/img/beetles.jpg", "/img/bat-trooper.jpg", "/img/dadandkids.jpeg"
+    ]
+const choosePic = () => {
+    var members = $('[id=member-pic]');
+    for(var i=0;i<members.length;i++){
+        var randomNum = Math.floor(Math.random() * familyPics.length);
+        $(members[i]).attr('src',familyPics[randomNum]);
+        }
+    }
+choosePic();
 
- $('.member-btn').click(function(){
-      $('#member-btn').toggle();
-      $('#member-form').toggle();
-     });
- 
+    var calendarEl = $('#calendar-profile');
+    calendarEl.fullCalendar({
+     header:
+         {
+              left: 'prev,next today',
+              center: 'title',
+              right: 'listMonth,listWeek,listDay'
+         },
+        defaultView: 'listWeek',
+        events: [
+        {
+            title: 'All Day Event',
+            start: '2021-04-20'
+        },
+        {
+            title: 'Long Event',
+            start: '2020-04-21',
+            end: '2020-04-24'
+        },
+        {
+            id: 999,
+            title: 'Repeating Event',
+            start: '2020-12-09T16:00:00'
+        }]});
+
+     $('.member-btn').click(function(){
+          $('#member-btn').toggle();
+          $('#member-form').toggle();
+         });
+
+
+
