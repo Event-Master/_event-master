c6ad5aa4a66921482f0e302840fb7384083d854f
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-04-21 22:55:50 -0400

    minor fix to app.js

--- Event-Master/src/main/resources/static/js/app.js
+++ Event-Master/src/main/resources/static/js/app.js
@@ -1,17 +1,9 @@
-var calendar = $('#calendar-main');
-
-var eventArray;
-$.get("/calendarEvents").then( function(result){
-  console.log("we got it to run");
-  console.log(result);
-//  for(var i = 0; i < result.length; i++) eventArray.push(result[i]);
-eventArray = result;
-  console.log(result[0].title)
-  }).catch(console.log);
-
-console.log(eventArray[0].title);
-
 $(document).ready(function() {
+var eventArray = [];
+$.get("/calendarEvents").then( function(result){
+    eventArray = result;
+
+    var calendar = $('#calendar-main');
     calendar.fullCalendar({
         header: {
             left: 'prev,next, today',
@@ -22,7 +14,10 @@ $(document).ready(function() {
         navLinks: true, // can click day/week names to navigate views
         editable: true,
         eventLimit: true, // allow "more" link when too many events
-        events: eventArray,
+        events: eventArray
+        });
+    }).catch(console.log);
+});
 //        events: [
 //            {
 //                title: eventArray[0].title,
@@ -32,63 +27,15 @@ $(document).ready(function() {
 //                title: 'Long Event',
 //                start: '2020-12-07',
 //                end: '2020-12-10'
-//            },
-//            {
-//                id: 999,
-//                title: 'Repeating Event',
-//                start: '2020-12-09T16:00:00'
-//            },
-//            {
-//                id: 999,
-//                title: 'Repeating Event',
-//                start: '2020-12-16T16:00:00'
-//            },
-//            {
-//                title: 'Conference',
-//                start: '2020-12-11',
-//                end: '2020-12-13'
-//            },
-//            {
-//                title: 'Meeting',
-//
-//                start: '2020-12-12T10:30:00',
-//                end: '2020-12-12T12:30:00'
-//            },
-//            {
-//                title: 'Lunch',
-//                start: '2020-12-12T12:00:00'
-//            },
-//            {
-//                title: 'Meeting',
-//                start: '2020-12-12T14:30:00'
-//            },
-//            {
-//                title: 'Happy Hour',
-//                start: '2020-12-12T17:30:00'
-//            },
-//            {
-//                title: 'Dinner',
-//                start: '2020-12-12T20:00:00'
-//            },
-//            {
-//                title: 'Birthday Party',
-//                start: '2020-12-13T07:00:00'
-//            },
-//            {
-//                title: 'Click for Google',
-//                url: 'https://google.com/',
-//                start: '2020-12-28'
-//            },
+//            },//
 //            {
 //                title: 'Christmas!',
 //                start: '2020-12-25',
 //                allDay: true
 //            }
 //        ]
-    });
 
 
-});
 //
 // calendar.addEvent({
 //    title: 'Add Event....',
